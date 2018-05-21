28fb9f1aa208dd698e6b4de73c0f098a73e33f89
s1260250 <s1260250@u-aizu.ac.jp>
2018-05-21 18:38:19 +0900

    Solve ITP_1_6_A

---
+++ ITP1_6/ITP_1_6_A.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+  int n;
+  int a[100];
+  scanf("%d", &n);
+  for(int i=0; i < n; i++){
+    scanf("%d", &a[i]);
+  }
+
+  for(int i=n-1; i >= 0; i--){
+    printf("%d", a[i]);
+    if(i)printf(" ");
+  }
+  printf("\n");
+  
+  return 0;
+}
