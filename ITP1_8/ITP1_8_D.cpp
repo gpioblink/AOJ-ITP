554ea38651fa49cac6457e04f31f81589ac3889c
Gpio Blink <gpioblink@wlan-napt-001.u-aizu.ac.jp>
2018-06-21 10:13:18 +0900

    Solve ITP1_8_*

--- ITP1_8/ITP1_8_D.cpp
+++ ITP1_8/ITP1_8_D.cpp
@@ -1,7 +1,28 @@
 #include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
 
 int main(){
-  char ring[1000];
+  string s,p;
+  int flag = 0;
+  getline(cin,s);
+  getline(cin,p);
+
+  string s2 = s+s;
   
+  for(int i=0; i<(int)s2.length(); i++){
+    for(int j=0; j<(int)p.length(); j++){
+      //cout << "Try :" << s2[i+j] << " " << p[j]<<endl;
+      if(s2[i+j]!=p[j]){
+	break;
+      }else if((int)p.length() == j+1){
+	flag = 1;
+      }
+    }
+    if(flag) break;
+  }
+  
+  if(flag) cout<<"Yes"<<endl;
+  else cout<<"No"<<endl;
 }
 
